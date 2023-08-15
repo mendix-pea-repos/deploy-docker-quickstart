8c0325a315bc63f2f7e5c5b2f8763df1eea6aa09
Sharath Sahadevan <sharath.sahadevan@mendix.com>
2023-08-15 16:26:14 -0500

    updates star.sh and image
    Run the hsql version

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 cd ~/dockerdemo/docker-mendix-buildpack/
 #make get-sample
 # sudo make build-image 
-sudo make run-container
+# sudo make run-container
+sudo make run-container-hsql
 
 sudo docker ps
 curl -X GET http://localhost:8080
